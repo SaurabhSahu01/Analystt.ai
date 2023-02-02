1ed320f6cb81877d00ef434f003c33f06c7ae4e1
SaurabhSahu01 <saurabhsahu794@gmail.com>
2023-02-03 02:44:20 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import { useState, useEffect } from "react";
 import List from "./Components/List";
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 function App() {
   const [userdata, setuserdata] = useState([]);
-  const [loading, setloading] = useState(true)
+  const [loading, setloading] = useState(true);
+  const [index, setindex] = useState(1);
+
+  function updateIndex(index) {
+    setindex(index);
+    console.log(index);
+  }
+
+  function indexInc() {
+    setindex((index) => {
+      if (index < 4) return index + 1;
+      else return index;
+    });
+  }
+
+  function indexDec() {
+    setindex((index) => {
+      if (index > 1) return index - 1;
+      else return index;
+    });
+  }
+
   async function userData() {
     await fetch("https://jsonplaceholder.typicode.com/users")
       .then(res => res.json())
@@ -14,12 +37,29 @@ function App() {
     userData();
   }, [])
   return (
-    <div className="m-10 h-[67vh] my-auto bg-red-500 overflow-y-hidden rounded-md">
-      {loading ? <p>loading...</p> : userdata.map((user, index) => {
-        if(index < 3)
-        return <List data = {user} key={user.id}></List>
-        else return <></>
-      })}
+    <div>
+      <p className="text-center text-3xl font-semibold">Analystt.ai Users List</p>
+      <div className="min-h-[90vh] my-auto overflow-y-scroll flex flex-col justify-around gap-5">
+        {loading ? <p className="font-bold text-3xl text-center text-red-500">Loading...</p> : userdata.map((user, ind) => {
+          return (ind + 1 > (3 * (index - 1)) && ind + 1 <= (3 * (index))) ? <List data={userdata[ind]} key={userdata[ind].id}></List> : <></>
+        })}
+      </div>
+      <div className="w-full fixed bottom-5 z-50">
+        <ul className="w-6/12 mx-auto flex justify-between items-center mt-2 cursor-pointer">
+          <ArrowBackIosIcon onClick={indexDec}></ArrowBackIosIcon>
+          {userdata.map((user, ind) => {
+            let times = userdata.length / 3;
+            if (userdata.length % 3 !== 0) {
+              times = times + 1;
+            }
+            if (ind !== 0 && ind <= times) {
+              return (ind === index) ? <li onClick={() => updateIndex(ind)} className="bg-red-500 px-[20px] rounded-full text-white font-semibold">{ind}</li> : <li onClick={() => updateIndex(ind)} className="font-semibold">{ind}</li>;
+            }
+            else return <></>;
+          })}
+          <ArrowForwardIosIcon onClick={indexInc}></ArrowForwardIosIcon>
+        </ul>
+      </div>
     </div>
   );
 }
--- src/Components/List.js
+++ src/Components/List.js
@@ -1,21 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react';
+import "./List.css";
+function List({ data }) {
 
-function List({data}) {
-  function clickhandler(){
+  const [visible, setvisible] = useState(false);
 
+  function clickHandler() {
+    setvisible((visible) => {
+      return (!visible);
+    });
   }
+
+
   return (
-    <div className='w-10/12 mx-auto  bg-slate-100 my-3 p-2 rounded-xl'>
-        <ul className='w-10/12 mx-auto h-[100%] my-auto flex justify-between items-center'>
-            <li>{data?.id}</li>
-            <li>{data?.address?.city}</li>
-            <li>{data?.address?.street}</li>
-            <button className='w-20 p-2 bg-red-500 rounded-xl text-yellow-50' onClick={clickhandler}>View</button>
-        </ul>
-        <div className='w-10/12 mx-auto rounded-xl bg-red-200'>
-            <p>Description</p>
-            <p>{data?.company?.catchPhrase}</p>
+    <div className='w-10/12 mx-auto  bg-slate-100 p-2 rounded-xl shadow-sm text-sm'>
+      <ul className='w-10/12 mx-auto h-[150px] my-auto grid grid-cols-5 items-center place-items-start gap-7'>
+        <li className='text-lg'>{data?.company?.name}</li>
+        <div>
+          <p className='font-semibold text-lg'>Contact</p>
+          <li>{data?.name}</li>
         </div>
+        <div>
+          <p className='font-semibold text-lg'>City</p>
+          <li>{data?.address?.city}</li>
+        </div>
+        <div>
+          <p className='font-semibold text-lg'>Street</p>
+          <li>{data?.address?.street}</li>
+        </div>
+        <button className='w-20 p-2 bg-red-500 rounded-xl text-yellow-50 font-semibold self-center place-self-end' onClick={clickHandler}>{!visible ? "Details" : "Hide"}</button>
+      </ul>
+      <div className={!visible ? 'flex flex-wrap w-9/12 mx-auto justify-between items-center hiddenDiv' : 'flex flex-wrap w-9/12 mx-auto justify-between items-center showDiv bg-white rounded-lg p-4 my-5 shadow-lg'}>
+        <div className='w-11/12 mx-auto'>
+          <div className='flex justify-between items-center'>
+            <div>
+              <p className="font-semibold text-lg">Description</p>
+              <p>{data?.company?.catchPhrase},</p>
+              <p>{data?.company?.bs}</p>
+            </div>
+            <div>
+              <p className="font-semibold text-lg">Website</p>
+              <p>{data?.website}</p>
+            </div>
+          </div>
+          <div className='w-full mx-auto flex justify-between items-center mt-5'>
+            <ul className='flex flex-col gap-4'>
+              <div>
+                <p className="font-semibold text-lg">Contact Person</p>
+                <p>{data?.name}</p>
+              </div>
+              <div>
+                <p className="font-semibold text-lg">Username</p>
+                <p>{data?.username}</p>
+              </div>
+            </ul>
+            <ul className='flex flex-col gap-3'>
+              <div>
+                <p className="font-semibold text-lg">Phone</p>
+                <p>{data?.phone}</p>
+              </div>
+              <div>
+                <p className="font-semibold text-lg">Address</p>
+                <p>{data?.address?.street}, {data?.address?.suite}, {data?.address?.city}, {data?.address?.zipcode}</p>
+              </div>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
